00b0fb323d8c8238364dbc9e18c2a8c4accb5892
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-08-22 21:16:31 +0700

    init project

---
+++ src/test/java/script/TestExample.java
@@ -0,0 +1,74 @@
+package script;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestExample {
+    // Shared between all tests in this class.
+    static Playwright playwright;
+    static Browser browser;
+
+    // New instance for each test method.
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        switch (System.getProperty("browser", "chromium")) {
+            case "chromium" -> browser = playwright.chromium().launch();
+            case "firefox" -> browser = playwright.firefox().launch();
+            case "webkit" -> browser = playwright.webkit().launch();
+            default -> throw new IllegalArgumentException("Unexpected browser name");
+        }
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                .setChannel("chrome")
+                .setHeadless(false));
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        context.tracing().start(new Tracing.StartOptions()
+                .setScreenshots(true)
+                .setSnapshots(true)
+                .setSources(true));
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+
+    @Test
+    void shouldClickButton() {
+        page.navigate("data:text/html,<script>var result;</script><button onclick='result=\"Clicked\"'>Go</button>");
+        page.locator("button").click();
+        assertEquals("Clicked", page.evaluate("result"));
+    }
+
+    @Test
+    void shouldCheckTheBox() {
+        page.setContent("<input id='checkbox' type='checkbox'></input>");
+        page.locator("input").check();
+        assertTrue((Boolean) page.evaluate("() => window['checkbox'].checked"));
+    }
+
+    @Test
+    void shouldSearchWiki() {
+        page.navigate("https://www.wikipedia.org/");
+        page.locator("input[name=\"search\"]").click();
+        page.locator("input[name=\"search\"]").fill("playwright");
+        page.locator("input[name=\"search\"]").press("Enter");
+        assertEquals("https://en.wikipedia.org/wiki/Playwright", page.url());
+    }
+}
